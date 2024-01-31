134fa350837f5acb6b0bfc6235167a34ea54f1c7
shv-gitman <sh.veum@gmail.com>
2024-01-31 22:06:02 +0100

    Updated Swagger Enum list

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using backend.Enums;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -9,20 +10,12 @@ using NetBackend.Models.User;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.MapType<DatabaseType>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Enum = Enum.GetNames(typeof(DatabaseType))
-                   .Select(name => new OpenApiString(name) as IOpenApiAny)
-                   .ToList()
-    });
-});
+builder.Services.AddSwaggerGen();
 
 // Add authentication (modified from https://youtu.be/ORzt0lks2H4?si=kXqRl7VUO9r9BPbN)
 builder.Services.AddAuthentication("Bearer")
