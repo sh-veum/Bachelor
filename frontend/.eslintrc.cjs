fbc8cbfeac3225067c61d4ccc6e80f3028df542b
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2024-02-01 19:34:44 +0100

    recreate frontend again using create-vue

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,14 +1,15 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
-    env: {
-      node: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:vue/vue3-recommended',
-      'prettier',
-    ],
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
   }
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-// import './style.css'
+// import './assets/main.css'
 import './assets/index.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
