9f5a9d064660cd4b970e75cbfc0c7c9e0c0912c7
shv-gitman <sh.veum@gmail.com>
2024-02-11 18:27:21 +0100

    Update CustomerView.vue

--- frontend/src/views/CustomerView.vue
+++ frontend/src/views/CustomerView.vue
@@ -3,5 +3,6 @@ import TheApiOverview from '@/components/TheApiOverview.vue'
 </script>
 
 <template>
+  <!-- Need to be logged in as a user and connected to backend to see -->
   <TheApiOverview class="ml-6" />
 </template>
