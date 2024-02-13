5aeb04b2cf8a82b35962d6ac6d3f50c5d898e264
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2024-02-13 17:54:45 +0100

    add table skeleton for keys created

---
+++ frontend/src/components/ThemeCollapsible.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
+import { ChevronsUpDown } from 'lucide-vue-next'
+import { ChevronsDown } from 'lucide-vue-next'
+import { ChevronsUp } from 'lucide-vue-next'
+
+defineProps<{
+  msg: string
+  // key: { name: string; themes: string[] }
+}>()
+
+const key = { name: 'Key 1', themes: ['Theme 1', 'Theme 2', 'Theme 3'] }
+
+const isOpen = ref(false)
+</script>
+
+<template>
+  <Collapsible v-model:open="isOpen" class="w-[350px] space-y-2">
+    <div class="flex items-center justify-between space-x-4 px-4">
+      <h4 class="text-sm font-semibold px-4 py-3">{{ key.themes.length }} themes</h4>
+      <CollapsibleTrigger as-child>
+        <Button variant="ghost" size="sm" class="w-9 p-0">
+          <ChevronsDown v-if="isOpen == false" class="h-4 w-4" />
+          <ChevronsUp v-if="isOpen == true" class="h-4 w-4" />
+          <span class="sr-only">Toggle</span>
+        </Button>
+      </CollapsibleTrigger>
+    </div>
+    <CollapsibleContent v-if="key.themes.length > 1" class="space-y-2">
+      <div v-for="theme in key.themes" class="rounded-md border px-4 py-3 font-mono text-sm">
+        {{ theme }}
+      </div>
+    </CollapsibleContent>
+  </Collapsible>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,43 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ThemeCollapsible from '@/components/ThemeCollapsible.vue'
+import { Button } from '@/components/ui/button'
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
+
+const keys = [
+  { name: 'Key 1', themes: ['Theme 1', 'Theme 2', 'Theme 3'] },
+  { name: 'Key 2', themes: ['Theme 1', 'Theme 2', 'Theme 3'] },
+  { name: 'Key 3', themes: ['Theme 1', 'Theme 2', 'Theme 3'] }
+]
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <Button class="mb-4">Create new key</Button>
+  <Table>
+    <TableCaption>A list of your created keys</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead>Name</TableHead>
+        <TableHead>Themes</TableHead>
+        <TableHead>Actions</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow v-for="key in keys">
+        <TableCell>{{ key.name }}</TableCell>
+        <TableCell>
+          <ThemeCollapsible :msg="'wtf'" />
+        </TableCell>
+        <TableCell><Button variant="destructive" @click=""> Delete </Button></TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
 </template>
