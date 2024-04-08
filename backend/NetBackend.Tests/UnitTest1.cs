9f9c3103feba50079ac57067fd8c8317dfa98c9d
shv-gitman <sh.veum@gmail.com>
2024-04-08 16:15:20 +0200

    Added .NET test project to docker

--- backend/NetBackend.Tests/UnitTest1.cs
+++ backend/NetBackend.Tests/UnitTest1.cs
@@ -5,6 +5,7 @@ public class UnitTest1
     [Fact]
     public void Test1Fails()
     {
+        // Uncomment to fail test to see that the docker container fails building
         // Assert.True(false);
     }
 }
