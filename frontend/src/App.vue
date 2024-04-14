4c2f8b355011f2e9181445aad883e10676e4802d
FennecFromSahara <143624395+FennecFromSahara@users.noreply.github.com>
2024-04-14 17:59:59 +0200

    add Toaster to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import TheNavigation from './components/TheNavigation.vue'
+import { Toaster } from '@/components/ui/toast'
 </script>
 
 <template>
   <TheNavigation class="mb-6 sticky top-0 left-0 right-0 z-20" />
 
   <RouterView />
+
+  <Toaster />
 </template>
